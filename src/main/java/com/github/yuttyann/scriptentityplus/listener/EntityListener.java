b1b8da359828a461f59333595caee7fd5f1a1a55
yuttyann44581 <misuto546@gmail.com>
2020-06-12 17:25:19 +0900

    [v1.0.2] エンティティが死亡した際にスクリプトを実行する機能を追加

--- src/main/java/com/github/yuttyann/scriptentityplus/listener/EntityListener.java
+++ src/main/java/com/github/yuttyann/scriptentityplus/listener/EntityListener.java
@@ -10,15 +10,15 @@ import com.github.yuttyann.scriptblockplus.script.ScriptType;
 import com.github.yuttyann.scriptblockplus.script.option.other.ScriptAction;
 import com.github.yuttyann.scriptblockplus.utils.StringUtils;
 import com.github.yuttyann.scriptblockplus.utils.Utils;
-import com.github.yuttyann.scriptentityplus.ArmType;
+import com.github.yuttyann.scriptentityplus.Main;
 import com.github.yuttyann.scriptentityplus.Permission;
-import com.github.yuttyann.scriptentityplus.item.ScriptConnection;
+import com.github.yuttyann.scriptentityplus.enums.ArmType;
+import com.github.yuttyann.scriptentityplus.enums.ToolMode;
 import com.github.yuttyann.scriptentityplus.json.ScriptEntity;
 import com.github.yuttyann.scriptentityplus.json.ScriptEntityInfo;
 import com.github.yuttyann.scriptentityplus.script.ScriptRead;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -26,35 +26,81 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EntityListener implements Listener {
+
+	public static final Set<Entity> TEMP_ENTITIES = new HashSet<>();
 
 	public static final String KEY_OFF = Utils.randomUUID();
 	public static final String KEY_ENTITY = Utils.randomUUID();
 	public static final String KEY_CLICK_ENTITY = Utils.randomUUID();
 
 	@EventHandler(priority = EventPriority.HIGH)
+	public void onHangingBreakByEntity(HangingBreakByEntityEvent event) {
+		if (event.getRemover() != null) {
+			damageEvent(event, event.getRemover(), event.getEntity(), 0.0D);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 		if (event.getCause() == EntityDamageEvent.DamageCause.VOID) {
 			return;
 		}
-		Entity damager = event.getDamager();
-		if (damager instanceof Player) {
-			Entity entity = event.getEntity();
-			ScriptEntityInfo info = new ScriptEntity(entity.getUniqueId()).getInfo();
-			if (info.isInvincible()) {
-				event.setCancelled(true);
+		 damageEvent(event, event.getDamager(), event.getEntity(), event.getDamage());
+	}
+
+	private void damageEvent(@NotNull Cancellable event, @NotNull Entity damager, @NotNull Entity entity, final double damage) {
+		ScriptEntity scriptEntity = new ScriptEntity(entity.getUniqueId());
+		if (!scriptEntity.has()) {
+			return;
+		}
+		ScriptEntityInfo info = scriptEntity.getInfo();
+		if (info.isInvincible()) {
+			event.setCancelled(true);
+		}
+		if (info.isProjectile()) {
+			if (!(damager instanceof ThrowableProjectile)) {
+				return;
 			}
-			if (info.getScripts().size() > 0) {
-				for (String script : info.getScripts()) {
-					String[] array = StringUtils.split(script, "|");
-					read((Player) damager, entity, array[1], array[0], Action.LEFT_CLICK_AIR);
+			damager = (Entity) ((ThrowableProjectile) damager).getShooter();
+		}
+		if (damager instanceof Player) {
+			ToolMode toolMode = ToolMode.NORMAL_SCRIPT;
+			if (!event.isCancelled()) {
+				if (entity instanceof LivingEntity) {
+					if (entity instanceof ArmorStand) {
+						entity.remove();
+					}
+					if (entity.isDead() || ((LivingEntity) entity).getHealth() - damage < 1.0D) {
+						toolMode = ToolMode.DEATH_SCRIPT;
+					}
+				} else {
+					toolMode = ToolMode.DEATH_SCRIPT;
 				}
+			}
+			try {
+				if (toolMode == ToolMode.DEATH_SCRIPT) {
+					TEMP_ENTITIES.add(entity = Main.getInstance().createArmorStand(entity.getLocation()));
+				}
+				if (info.getScripts(toolMode).size() > 0) {
+					for (String script : info.getScripts(toolMode)) {
+						String[] array = StringUtils.split(script, "|");
+						read((Player) damager, entity, array[1], array[0], Action.LEFT_CLICK_AIR);
+					}
+				}
+			} finally {
+				TEMP_ENTITIES.forEach(Entity::remove);
+				TEMP_ENTITIES.clear();
 			}
 		}
 	}
@@ -86,11 +132,11 @@ public class EntityListener implements Listener {
 			objectMap.put(KEY_CLICK_ENTITY, entity);
 			ItemStack main = player.getInventory().getItemInMainHand();
 			ItemStack off = player.getInventory().getItemInOffHand();
-			if (ScriptConnection.is(main) && Permission.TOOL_SCRIPT_CONNECTION.has(player)) {
+			if (ToolMode.has(main) && Permission.TOOL_SCRIPT_CONNECTION.has(player)) {
 				ArmType.MAIN_HAND.swingAnimation(player);
 				ItemAction.run(main, player, Action.RIGHT_CLICK_AIR, entity.getLocation(), true, player.isSneaking());
 				event.setCancelled(true);
-			} else if (ScriptConnection.is(off) && Permission.TOOL_SCRIPT_CONNECTION.has(player)) {
+			} else if (ToolMode.has(off) && Permission.TOOL_SCRIPT_CONNECTION.has(player)) {
 				try {
 					objectMap.put(KEY_OFF, true);
 					ArmType.OFF_HAND.swingAnimation(player);
@@ -100,12 +146,13 @@ public class EntityListener implements Listener {
 				}
 				event.setCancelled(true);
 			} else {
-				ScriptEntity scriptEntity = new ScriptEntity(entity.getUniqueId());
-				ScriptEntityInfo info = scriptEntity.getInfo();
-				if (info.getScripts().size() > 0) {
-					for (String script : info.getScripts()) {
-						String[] array = StringUtils.split(script, "|");
-						read(player, entity, array[1], array[0], Action.RIGHT_CLICK_AIR);
+				ScriptEntityInfo info = new ScriptEntity(entity.getUniqueId()).getInfo();
+				if (info.getScripts(ToolMode.NORMAL_SCRIPT).size() > 0) {
+					if (!info.isProjectile()) {
+						for (String script : info.getScripts(ToolMode.NORMAL_SCRIPT)) {
+							String[] array = StringUtils.split(script, "|");
+							read(player, entity, array[1], array[0], Action.RIGHT_CLICK_AIR);
+						}
 					}
 					event.setCancelled(true);
 				}
