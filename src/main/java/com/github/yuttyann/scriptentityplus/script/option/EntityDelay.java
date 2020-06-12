65b43087518249252a87a0972c2dd57d2d1e2336
yuttyann44581 <misuto546@gmail.com>
2020-06-12 17:24:15 +0900

    [v1.0.2] エンティティが死亡した際に実行する場合に正常に動作しないため、対策

--- src/main/java/com/github/yuttyann/scriptentityplus/script/option/EntityDelay.java
+++ src/main/java/com/github/yuttyann/scriptentityplus/script/option/EntityDelay.java
@@ -4,15 +4,18 @@ import com.github.yuttyann.scriptblockplus.file.config.SBConfig;
 import com.github.yuttyann.scriptblockplus.manager.EndProcessManager;
 import com.github.yuttyann.scriptblockplus.script.option.Option;
 import com.github.yuttyann.scriptblockplus.utils.StringUtils;
+import com.github.yuttyann.scriptentityplus.Main;
 import com.github.yuttyann.scriptentityplus.listener.EntityListener;
 import com.github.yuttyann.scriptentityplus.script.EntityOption;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 
 public class EntityDelay extends EntityOption implements Runnable {
 
     private Entity entity;
+    private Location location;
     private boolean unSaveExec;
 
     public EntityDelay() {
@@ -45,6 +48,7 @@ public class EntityDelay extends EntityOption implements Runnable {
                 getMapManager().putDelay(getUniqueId(), getScriptType(), blockCoords);
             }
             this.entity = getEntity();
+            this.location = entity.getLocation();
             Bukkit.getScheduler().runTaskLater(getPlugin(), this, Long.parseLong(array[0]));
         }
         return false;
@@ -56,8 +60,21 @@ public class EntityDelay extends EntityOption implements Runnable {
             getMapManager().removeDelay(getUniqueId(), getScriptType(), getScriptLocation().getFullCoords());
         }
         if (getSBPlayer().isOnline()) {
-            getSBRead().put(EntityListener.KEY_ENTITY, entity);
-            getSBRead().read(getScriptIndex() + 1);
+            if (entity.isDead()) {
+                EntityListener.TEMP_ENTITIES.add(entity = Main.getInstance().createArmorStand(location));
+            }
+            try {
+                getSBRead().put(EntityListener.KEY_ENTITY, entity);
+                getSBRead().read(getScriptIndex() + 1);
+            } finally {
+                EntityListener.TEMP_ENTITIES.removeIf(e -> {
+                    boolean result = e.getUniqueId().equals(entity.getUniqueId());
+                    if (result) {
+                        entity.remove();
+                    }
+                    return result;
+                });
+            }
         } else {
             EndProcessManager.forEach(e -> e.failed(getSBRead()));
         }
